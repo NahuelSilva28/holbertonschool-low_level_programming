562eabff0d0979a6bcc9efa932487594d5e4f9a6
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-27 08:01:45 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ **main - Entry point
+ **printf bytes
+ **Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	printf("\nSize of char: %lu bytes", sizeof (char));
+	return (0);
+}      
