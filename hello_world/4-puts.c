aa95f09c7daa84c97baadbe95de9ad6dc90ebcae
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-27 06:54:06 -0700

    put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 int main (void){
-gcc -s puts ("\"Programming is like building a multilingual puzzle");}
-return = 0;
+gcc puts ("\"Programming is like building a multilingual puzzle");
+return (0);}
