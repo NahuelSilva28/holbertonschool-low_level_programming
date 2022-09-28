1989daad584a5626a3766fafa361d92418a8c7ae
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-28 09:01:56 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* If and else
+* Return: Always 0 (Success)
+*/
+int main()
+for(int x = 'A'; x <= 'Z'; x++)
+{
+    putchar(tolower(x));
+}
+return (0)
+}
