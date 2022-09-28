2978abe399ae395bacc3a2070fb02425f2164924
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-28 16:13:01 -0700

    bet

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints all single digit 
- * numbers of base 10 starting from 0
- * 
- * Primtf
- * Return: Always 0 (Success)
- */
+* main - prints all single digit
+* numbers of base 10 starting from 0
+* Primtf
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i;
