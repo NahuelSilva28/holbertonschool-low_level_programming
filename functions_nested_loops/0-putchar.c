3cb90c86c9a4c5fc31270b8847309a76420bf45e
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-29 06:30:55 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Write a program that prints _putchar
+*
+*
+* Return: (0) Success
+*/
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
