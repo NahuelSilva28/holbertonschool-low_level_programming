eaf4fbf017179be3124ba486bf2e68b45936209f
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-29 07:16:41 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - void print_alphabet(void);
+*
+*
+* Return: (0) Success
+*/
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
