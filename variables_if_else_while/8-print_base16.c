6ff6c6433693d53184af38f79cd2881317af704c
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-29 05:41:25 -0700

    bety

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,11 +10,10 @@ int main(void)
 	int n;
 	char s;
 
-		for (n = 0; n < 10; n++)
-	putchar((n % 10) + '0');
-		for (s = 'a'; s <= 'f'; s++)
-	putchar(s);
-	putchar('\n');
-
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	for (s = 'a'; s <= 'f'; s++)
+		putchar(s);
+		putchar('\n');
 return (0);
 }
