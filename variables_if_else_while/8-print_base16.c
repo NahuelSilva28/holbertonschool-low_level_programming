ca335a3a1e5144beb13cf904a0e2ad2a10f5e4cc
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-29 05:39:18 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - base diez y seis
+*
+*
+* Return: (0) Success
+*/
+int main(void)
+{
+	int n;
+	char s;
+
+		for (n = 0; n < 10; n++)
+	putchar((n % 10) + '0');
+		for (s = 'a'; s <= 'f'; s++)
+	putchar(s);
+	putchar('\n');
+
+return (0);
+}
