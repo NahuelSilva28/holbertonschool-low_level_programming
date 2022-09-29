a52168f05af42431a27b5331009ece48e9ad1dfa
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-09-29 07:13:13 -0700

    main

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,3 +10,15 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
