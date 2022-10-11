a26b2493331b7905ab9c18862aafaacd7df1baf3
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-11 07:29:04 -0700

    1mem

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -7,14 +7,18 @@
  *@n: The number of bytes to copy from @src.
  *Return: A pointer to the destination buffer @dest.
  **/
-void *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	int i = 0;
+	int j = 0;
 
-	for (index = 0; index < n; index++)
-	destination[index] = source[index];
+	while (n > 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		n--;
+	}
 
 	return (dest);
 }
