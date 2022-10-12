19ec736c27ed795d79894ecb879b74c76ff72af6
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-12 05:23:09 -0700

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strpbrk - searching a string
+ *@s: the string
+ *@accept: accept set of bytes
+ *Return: a pointer to the bytes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+return (NULL);
+}
