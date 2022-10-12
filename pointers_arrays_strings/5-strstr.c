9773460c699bef1ca02ba9d6bc2b7f1a4894d158
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-12 06:00:35 -0700

    str5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *_strstr - locate and return pointer to first occurence of substring
+ *@haystack: string
+ *@needle: target substrin
+ *Return: pointer to index of string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, j, x;
+
+if (needle[0] == '\0')
+	return (haystack);
+
+while (haystack[i] != '\0')
+{
+
+	if (haystack[i] == needle[0])
+	{
+		x = i, j = 0;
+		while (needle[j] != '\0')
+		{
+			if (haystack[x] == needle[j])
+				x++, j++;
+			else
+							break;
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	i++;
+}
+return (NULL); /* No match */
+}
