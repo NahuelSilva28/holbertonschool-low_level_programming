3ddb372004c0f3ae6f12bfac0f602fc35e71a964
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-17 10:38:27 -0700

    main

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,49 +1,11 @@
+#ifndef main_h
+#define main_h
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <float.h>
-#include <limits.h>
-#include <locale.h>
-#include <setjmp.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stddef.h>
+#include <stdio.h>
 #include <string.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * _isupper - function checks whether a character is an uppercase alphabet
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -54,121 +16,34 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-julien@ubuntu:~/Static Librairies$ ar -t libmy.a 
-0-isupper.o
-0-memset.o
-0-strcat.o
-1-isdigit.o
-1-memcpy.o
-1-strncat.o
-100-atoi.o
-2-strchr.o
-2-strlen.o
-2-strncpy.o
-3-islower.o
-3-puts.o
-3-strcmp.o
-3-strspn.o
-4-isalpha.o
-4-strpbrk.o
-5-strstr.o
-6-abs.o
-9-strcpy.o
-_putchar.o
-julien@ubuntu:~/Static Librairies$ nm libmy.a 
-
-0-isupper.o:
-0000000000000000 T _isupper
-
-0-memset.o:
-0000000000000000 T _memset
-
-0-strcat.o:
-0000000000000000 T _strcat
-
-1-isdigit.o:
-0000000000000000 T _isdigit
-
-1-memcpy.o:
-0000000000000000 T _memcpy
-
-1-strncat.o:
-0000000000000000 T _strncat
-
-100-atoi.o:
-0000000000000000 T _atoi
-
-2-strchr.o:
-0000000000000000 T _strchr
-
-2-strlen.o:
-0000000000000000 T _strlen
-
-2-strncpy.o:
-0000000000000000 T _strncpy
-
-3-islower.o:
-0000000000000000 T _islower
-
-3-puts.o:
-                 U _putchar
-0000000000000000 T _puts
-
-3-strcmp.o:
-0000000000000000 T _strcmp
-
-3-strspn.o:
-0000000000000000 T _strspn
-
-4-isalpha.o:
-0000000000000000 T _isalpha
-
-4-strpbrk.o:
-0000000000000000 T _strpbrk
-
-5-strstr.o:
-0000000000000000 T _strstr
-
-6-abs.o:
-0000000000000000 T _abs
-
-9-strcpy.o:
-0000000000000000 T _strcpy
-
-_putchar.o:
-0000000000000000 T _putchar
-                 U write
-julien@ubuntu:~/Static Librairies$ cat main.c 
-#include "main.h"
-
-int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
-}
-
-
+#endif
