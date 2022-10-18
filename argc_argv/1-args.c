e0fcca9578b6643d0526320fd58e243b14073fe9
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-18 05:46:10 -0700

    arg

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,11 +7,7 @@
  **/
 int main(int argc, char *argv[])
 {
-int i;
-printf("%d\n", argc);
-for (i = 1; i < argc; i++)
-{
-printf("%s", argv[i]);
-}
+argv = argv;
+printf("%d\n", argc - 1);
 return (0);
 }
