dd0e9d55514bba958844c89b4fed868396b567fc
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-18 05:33:40 -0700

    whatsismyname?

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *main - Write a program that prints its name, followed by a new line.
+ *@argc: arg count
+ *@argv: array of pointers
+ *Return: 0
+**/
+int main(int argc, char **argv)
+{
+	argc = argc;
+	printf("%f\n", *argv);
+return (0);
+}
