79b7e8ffc041689effcda8ed9fb645741e4cb3f0
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-25 06:52:24 -0700

    arra

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*array_range - range
+*@min: minimo
+*@max: max
+*Return: pointer
+**/
+int *array_range(int min, int max)
+{
+	int *p
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+	size  = (max - min) + 1;
+	p = malloc(size * sizeof(*p));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size && mim <= max; i++, min++)
+		*(p + i) = min;
+	return (p);
+}
