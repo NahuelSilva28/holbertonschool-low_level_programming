84178b2df9f131247a298216031a3973d0579576
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-25 06:34:26 -0700

    call

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_calloc - call ok
+*@nmemb: n
+*@size: size
+*Return: 0
+**/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *n;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	n = malloc(nmemb * size);
+	if (n == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * sice; i++)
+		*(n + i) = 0;
+	return ((void *)n);
+}
