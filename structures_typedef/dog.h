cec6e5e9570806206a4922a43514a858d4f29cec
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-10-30 16:23:17 -0700

    d

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,33 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
-*init_dog - initializes a struct of type dog
-*@d: pointer to structure
-*@name: pointer to the name of the dog
-*@age: age of dog
-*@owner: pointer to dog owner
-*
-*Return: void
-**/
-void init_dog(struct dog *d, char *name, float age, char *owner)
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
