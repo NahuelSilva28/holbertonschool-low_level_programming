df15c0d52dd631928afbd0c8175e4123901fdcb4
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-01 05:38:10 -0700

    varia

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdrag.h>
+#include <stdarg.h>
 int _strlen(char *s);
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
