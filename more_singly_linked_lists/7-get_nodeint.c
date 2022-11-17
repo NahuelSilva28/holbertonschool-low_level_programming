0c885d531ef4a72ef799b9a77f5bfad623c58653
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-17 12:28:42 -0800

    fixbetty

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -13,8 +13,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (i < index)
 	{
 		if (head->next == NULL)
-			return(NULL);
-		heaf = head->next;
+			return (NULL);
+		head = head->next;
 		i++;
 	}
 	return (head);
