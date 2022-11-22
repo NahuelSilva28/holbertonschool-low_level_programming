2f89fb2de6ced6d4ec71d7f481cb5ffc06460a91
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-22 08:52:33 -0800

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 
