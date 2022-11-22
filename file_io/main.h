3ddd8f8519ca72b4fb289ab0de8de94d9e90d3c3
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-22 08:37:50 -0800

    libmain

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <sys/types-h>
+#include <sys/stat.h>
+#include <fcnt1.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 
 int _putchar(char c);
 
