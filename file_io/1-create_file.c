e3a046c674aa7ad820d4eb9afd40259a1436bd10
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-23 04:19:21 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,3 +2,14 @@
 int create_file(const char *filename, char *text_content)
 {
 
+	int fd, lenght;
+	ssize_t res_write;
+
+	if (filname == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WEONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		
