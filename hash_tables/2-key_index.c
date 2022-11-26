3ed94804c9062960d6e70193bc25e7c398733dfc
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-26 12:14:17 -0800

    key

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+*key_index - index with function djb2 and size
+*@size: size the arrays the hash table
+*@key: key the index
+*Return: the index at which the key/value
+**/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
