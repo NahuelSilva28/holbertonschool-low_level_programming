470fa254270667326bd2475c30aa7f8576a2b339
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-17 05:10:57 -0800

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Call putchar function for each character in "_putchar\n"
+* Return: Exit the program with a success status
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+return (0);
+}
+
