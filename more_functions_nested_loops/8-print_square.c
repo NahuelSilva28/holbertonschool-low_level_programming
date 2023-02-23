fb5eaf61f283e5bd630d8c0bff8a00f69b527f85
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-23 04:54:32 -0800

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_square - Prints a square using the character #.
+*@size: The size of the square.
+*
+*Return: void.
+*/
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
