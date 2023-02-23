43635553eacf47796fbfe4bdfcbc0660224623df
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-23 04:30:55 -0800

    -.-

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*print_line - straight line
-*
-*/
+ *print_line - draws a straight line 
+ *@n: number of times the character '_' should be printed
+ */
 void print_line(int n)
 {
-	while (n--)
+	while (n-- > 0)
 		_putchar('_');
 
 	_putchar('\n');
