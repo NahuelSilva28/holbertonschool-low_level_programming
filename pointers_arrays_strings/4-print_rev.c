3d880d77c94030752fe442d52bb01f78136e806c
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-27 04:58:28 -0800

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/**
+ * print_rev - Prints a string in reverse
+ * @s: Pointer to the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int n;
+
+	while (*(s + len) != '\0')
+	{
+	len++;
+	}
+
+	for (int n = len - 1; n >= 0; n--)
+	{
+		_putchar(*(s + n));
+	}
+
+_putchar('\n');
+}
