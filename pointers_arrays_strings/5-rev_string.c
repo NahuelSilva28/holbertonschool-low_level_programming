84407439055dc9541f5be013b64ce8d7a0b7e3ea
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-27 08:51:04 -0800

    revs

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: char
+*/
+void rev_string(char *s)
+{
+	int a, b, tmp, len;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	len = a;
+	a--;
+	
+	while (b < len / 2)
+
+	{
+		tmp = s[b];
+		s[b] = s[a];
+		s[a] = tmp;
+		a--;
+		b++;
+	}
+}
