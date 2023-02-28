2bc2bc54c12836576b9061d6d777b42b775261e1
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-28 07:59:06 -0800

    push

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_memset - Fills memory with a constant byte
+*@s: Pointer to memory area
+*@b: Constant byte
+*@n: Number of bytes to fill
+*Return: Pointer to memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+while (n--)
+*(s + n) = b;
+
+return (s);
+}
