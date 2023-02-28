88b59f1a22bcffec8858a07731a4a58d7ce09caf
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-02-28 04:15:47 -0800

    btu

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,25 +6,25 @@
  **/
 int _atoi(char *s)
 {
-    int sign = 1;
-    unsigned int num = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-    while (*s)
-    {
-        if (*s >= '0' && *s <= '9')
-        {
-            num = (num * 10) + (*s - '0');
-        }
-        else if (*s == '-')
-        {
-            sign = -sign;
-        }
-        else if (num > 0)
-        {
-            break;
-        }
-        s++;
-    }
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+	{
+		num = (num * 10) + (*s - '0');
+	}
+		else if (*s == '-')
+	{
+		sign = -sign;
+	}
+		else if (num > 0)
+	{
+		break;
+	}
+		s++;
+	}
 
-    return (sign * num);
+return (sign * num);
 }
