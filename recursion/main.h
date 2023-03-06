a9ecc667084cce4e21aa0dc09815f5e4e5d3a73b
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-06 13:02:26 -0800

    m

--- recursion/main.h
+++ recursion/main.h
@@ -14,7 +14,15 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <string.h>
+#include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
