51f69d6336280f7b668052a498b48c8237e57c0b
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-06 12:57:59 -0800

    main

--- recursion/main.h
+++ recursion/main.h
@@ -47,6 +47,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void _puts_recursion(char *s);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
