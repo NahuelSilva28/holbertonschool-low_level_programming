e686229a3574094175addedf85db968a784d59a5
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-09 07:14:49 -0800

    argcv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<main.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ *@argc: arg count
+ *@argv: arg vector
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+argc = argc;
+printf("%s\n", *argv);
+return (0);
+}
