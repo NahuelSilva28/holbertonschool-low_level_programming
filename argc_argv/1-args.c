9c76bf57dbee27c9e745af897ebbbefdac27be81
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-11 08:05:46 -0800

    args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,17 +4,11 @@
 *main - program that prints the number of arguments
 *@argc: argument count
 *@argv: vector
-*Return: 0 on success, 1 on failure
+*Return: 0
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int num_arguments;
-if (argc < 2)
-{
-printf("Usage: %s <argument1> <argument2> ... <argumentN>\n", argv[0]);
-return 1;
-}
-num_arguments = argc - 1;
-printf("Number of arguments: %d\n", num_arguments);
-return 0;
+	argv = argv;
+		printf("%d\n", argc - 1);
+	return (0);
 }
