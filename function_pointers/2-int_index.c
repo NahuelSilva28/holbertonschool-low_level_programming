4147c6de036b7782a9202d1f3fc76d185c409fbc
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-24 11:04:16 -0700

    index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+* int_index - searches for an integer in an array using a comparison function
+* @array: the array
+* @size: size of the array
+* @cmp: the comparison function to use
+*
+* Return: -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+if (array && cmp && size > 0)
+{
+for (int i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+}
+return (-1);
+}
