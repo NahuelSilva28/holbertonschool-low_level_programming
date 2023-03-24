7c0578659be8a67cd14193949f4cbc80a090773c
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-24 13:45:39 -0700

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,16 +8,32 @@
  */
 int main(int argc, char *argv[])
 {
-register int i, j;
-int (*fptr)(int, int);
+int num1, num2, result;
+int (*op_func)(int, int);
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
-	fptr = get_op_func(argv[2]);
-	if (!fptr)
-		printf("Error\n"), exit(99);
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-	printf("%i\n", fptr(i, j));
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 return (0);
 }
