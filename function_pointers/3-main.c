7ff6a5d6a9ba8ccbda16775348cf616997af7e68
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-25 13:21:31 -0700

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
 int num1, num2;
 int (*func_ptr)(int, int);
 
-// Check if there are enough arguments
+
 if (argc != 4)
 {
 printf("Error\n");
 return (98);
 }
 
-// Get the correct arithmetic function
+
 func_ptr = get_op_func(argv[2]);
 if (func_ptr == NULL)
 {
@@ -26,11 +26,11 @@ printf("Error\n");
 return (99);
 }
 
-// Convert string arguments to integers
+
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 
-// Perform the arithmetic operation and print the result
+
 printf("%d\n", func_ptr(num1, num2));
 
 return (0);
