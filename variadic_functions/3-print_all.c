918994b0fcf0e059b1433fa4540f265670756033
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-26 12:38:36 -0700

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,61 +1,50 @@
 #include "variadic_functions.h"
 
 /**
-* print_all - prints all
-* @format: arguments passed to the function
+* print_all - prints any combination of c, i, f, or s types
+* @format: list of types of arguments passed to the function
 */
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0, j = 0;
-	char *str;
-	const char t_arg[] = "cifs";
-	char arg;
+va_list valist;
+unsigned int i = 0, j, c = 0;
+char *str;
+const char t_arg[] = "cifs";
 
-va_start(args, format);
-
-while (format && format[i])
-{
-arg = format[i];
-j = 0;
-
-while (t_arg[j])
-{
-if (arg == t_arg[j])
-{
-switch (arg)
-{
-case 'c':
-printf("%c", va_arg(args, int));
-break;
-case 'i':
-printf("%d", va_arg(args, int));
-break;
-case 'f':
-printf("%f", va_arg(args, double));
-break;
-case 's':
-str = va_arg(args, char *);
-if (!str)
-printf("(nil)");
-else
-printf("%s", str);
-break;
-}
-
-if (format[i + 1])
-printf(", ");
-
-break;
-}
-
-j++;
-}
-
-i++;
-}
-
-printf("\n");
-va_end(args);
+	va_start(valist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
 }
